21b984bd19c075c4b6536a7102435f872ee57fa3
MatheusLacerra <matheus.lacerra@edu.unipar.br>
2022-03-20 11:04:53 -0300

    pegando url pós upload de vídeo

--- app/Http/Controllers/ControllerVimeo.php
+++ app/Http/Controllers/ControllerVimeo.php
@@ -8,26 +8,48 @@ use Vimeo\Laravel\VimeoManager;
 use Vimeo\Vimeo;
 
 
-class ControllerVimeo extends Controller{
 
-    public function create(){
+class ControllerVimeo extends Controller
+{
+
+    public function create()
+    {
         return view('create');
     }
 
-    public function store(Request $request, VimeoManager $vimeo){
+    public function store(Request $request, VimeoManager $vimeo)
+    {
+
+
+
+        // $client = new Vimeo("{client_id}", "{client_secret}", "{access_token}");
+        $client = new Vimeo(env('VIMEO_CLIENT'), env('VIMEO_SECRET'), env('VIMEO_ACCESS'));
+
+        $response = $client->request("/videos/690202507" . '?fields=link');
+        echo "Your video link is: " . $response['body']['link'];
+
+        dd($response['body']['link']);
 
         $request->validate([
-            'video'=>'required|mimetypes:video/mp4,video/mpeg,video/quicktime|max:60000'
+            'video' => 'required|mimetypes:video/mp4,video/mpeg,video/quicktime|max:60000'
         ]);
 
 
         $uri = $vimeo->upload($request->video, [
-            'name'=> $request->title
+            'name' => $request->title
         ]);
+        // dd($uri);
 
-
-        dd($uri);
-
+        // do {
+        //     $response = $client->request($uri . '?fields=transcode.status');
+        //     if ($response['body']['transcode']['status'] === 'complete') {
+        //         print 'Your video finished transcoding.';
+        //     } elseif ($response['body']['transcode']['status'] === 'in_progress') {
+        //         print 'Your video is still transcoding.';
+        //     } else {
+        //         print 'Your video encountered an error during transcoding.';
+        //     }
+        // } while ($response['body']['transcode']['status'] !== 'complete');
 
     }
 }
